8b045f8a4ab94d705974a55264454c50e4493b56
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2026-01-23 23:42:31 -0500

    Handle error returned by http.ListenAndServe.

--- main.go
+++ main.go
@@ -10,5 +10,8 @@ func main() {
 	r := routes.Register()
 
 	log.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", r)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
