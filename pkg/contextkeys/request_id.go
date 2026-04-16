e4acffbd56fa40ec1e0418717ea8f559835477a5
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2026-04-15 22:27:44 -0500

    Add godoc for RequestIDKeyType.

--- pkg/contextkeys/request_id.go
+++ pkg/contextkeys/request_id.go
@@ -1,5 +1,7 @@
 package contextkeys
 
+// RequestIDKeyType is the context key type used to store
+// request IDs in a context.Context.
 type RequestIDKeyType struct{}
 
 var RequestIDKey = RequestIDKeyType{}
