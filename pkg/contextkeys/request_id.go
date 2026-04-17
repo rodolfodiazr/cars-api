35d7569053aca26172c8c254dfc01aea5afe815e
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2026-04-16 22:22:46 -0500

    Add godoc for RequestIDKey.

--- pkg/contextkeys/request_id.go
+++ pkg/contextkeys/request_id.go
@@ -4,4 +4,6 @@ package contextkeys
 // request IDs in a context.Context.
 type RequestIDKeyType struct{}
 
+// RequestIDKey is the context key used to store and retrieve
+// the request ID associated with an HTTP request.
 var RequestIDKey = RequestIDKeyType{}
