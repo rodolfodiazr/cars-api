3dfa071ab941f1bb1b4d19d906305499811e15a1
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2026-05-21 19:38:01 -0500

    Add a unit test.

--- models/car_test.go
+++ models/car_test.go
@@ -6,6 +6,126 @@ import (
 	"time"
 )
 
+func Test_ValidateForUpdate(t *testing.T) {
+	validMileage := int64(10000)
+	validPrice := int64(25000)
+	currentYear := time.Now().Year()
+
+	validCar := Car{
+		ID:       "1",
+		Make:     "Toyota",
+		Model:    "Corolla",
+		Color:    "Black",
+		Category: "Sedan",
+		Year:     currentYear,
+		Mileage:  &validMileage,
+		Price:    &validPrice,
+	}
+
+	tests := []struct {
+		name    string
+		car     Car
+		wantErr error
+	}{
+		{
+			name:    "should succeed for valid car",
+			car:     validCar,
+			wantErr: nil,
+		},
+		{
+			name:    "should fail when ID is missing",
+			car:     Car{},
+			wantErr: ErrCarIDRequiredForUpdate,
+		},
+		{
+			name: "should fail when make is blank",
+			car: func() Car {
+				c := validCar
+				c.Make = ""
+				return c
+			}(),
+			wantErr: ErrCarMakeRequired,
+		},
+		{
+			name: "should fail when model is blank",
+			car: func() Car {
+				c := validCar
+				c.Model = ""
+				return c
+			}(),
+			wantErr: ErrCarModelRequired,
+		},
+		{
+			name: "should fail when color is blank",
+			car: func() Car {
+				c := validCar
+				c.Color = ""
+				return c
+			}(),
+			wantErr: ErrCarColorRequired,
+		},
+		{
+			name: "should fail when category is blank",
+			car: func() Car {
+				c := validCar
+				c.Category = ""
+				return c
+			}(),
+			wantErr: ErrCarCategoryRequired,
+		},
+		{
+			name: "should fail when year is zero",
+			car: func() Car {
+				c := validCar
+				c.Year = 0
+				return c
+			}(),
+			wantErr: ErrInvalidYear,
+		},
+		{
+			name: "should fail when year is in the future",
+			car: func() Car {
+				c := validCar
+				c.Year = currentYear + 1
+				return c
+			}(),
+			wantErr: ErrInvalidYear,
+		},
+		{
+			name: "should fail when mileage is negative",
+			car: func() Car {
+				c := validCar
+				mileage := int64(-1)
+				c.Mileage = &mileage
+				return c
+			}(),
+			wantErr: ErrInvalidMileage,
+		},
+		{
+			name: "should fail when price is negative",
+			car: func() Car {
+				c := validCar
+				price := int64(-1)
+				c.Price = &price
+				return c
+			}(),
+			wantErr: ErrInvalidPrice,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Act
+			err := tt.car.ValidateForUpdate()
+
+			// Assert
+			if !errors.Is(err, tt.wantErr) {
+				t.Fatalf("expected error %v, got %v", tt.wantErr, err)
+			}
+		})
+	}
+}
+
 func Test_Car_Validate(t *testing.T) {
 	currentYear := time.Now().Year()
 
