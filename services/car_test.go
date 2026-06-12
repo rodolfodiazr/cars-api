277a743c2e07aaa12d125b38eb11dff3f72f758b
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2026-06-11 23:15:15 -0500

    Add a test case.

--- services/car_test.go
+++ services/car_test.go
@@ -119,6 +119,15 @@ func TestDefaultCarService_Find(t *testing.T) {
 		if err == nil {
 			t.Fatal("expected error but got nil")
 		}
+
+		var serviceError *e.ServiceError
+		if !errors.As(err, &serviceError) {
+			t.Fatalf("expected ServiceError, got %T", err)
+		}
+
+		if serviceError.Code != e.CodeInternalError {
+			t.Fatalf("expected INTERNAL_ERROR, got %v", serviceError.Code)
+		}
 	})
 }
 
