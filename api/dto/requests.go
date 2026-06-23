2c73308e041e4bae2efefb60afacfc8eba30d160
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2026-06-22 21:13:50 -0500

    Add a minor change.

--- api/dto/requests.go
+++ api/dto/requests.go
@@ -4,15 +4,15 @@ package dto
 // When used for updates, ALL fields must be provided.
 // Partial updates are not supported.
 type CarUpsertRequest struct {
-	Make     string  `json:"make"`
-	Model    string  `json:"model"`
-	Package  *string `json:"package,omitempty"`
-	Color    string  `json:"color"`
-	Category string  `json:"category"`
-	Year     int     `json:"year"`
+	Make     string `json:"make"`
+	Model    string `json:"model"`
+	Color    string `json:"color"`
+	Category string `json:"category"`
+	Year     int    `json:"year"`
 
-	Mileage *int64 `json:"mileage,omitempty"`
-	Price   *int64 `json:"price,omitempty"`
+	Package *string `json:"package,omitempty"`
+	Mileage *int64  `json:"mileage,omitempty"`
+	Price   *int64  `json:"price,omitempty"`
 }
 
 type CreateCarRequest = CarUpsertRequest
